0af8ac79fba07bf2d58201cf4511f1dd16122973
unknown <Yaya-sponge-bob@live.fr>
2023-02-14 21:54:38 +0100

    shi 7aja bayna

--- src/layouts/public/Public.jsx
+++ src/layouts/public/Public.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Menu from '../../components/menu/Menu'
 import { Outlet } from 'react-router-dom'
+import Footer from '../../components/footer/Footer'
 
 export default function Public() {
   return (
     <>
         <Menu/>
         <Outlet/>
+        <Footer/>
     </>
   )
 }
